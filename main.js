99f9a683548c808b5f2807eebe8eb0da30a436bc
Xuamaqo <151995495+Xuamaqo@users.noreply.github.com>
2025-11-14 18:40:50 +0100

    made changes, deleted some stuff

--- main.js
+++ main.js
@@ -1,65 +1,37 @@
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+const resultDiv = document.querySelector('#results');
+
 const choice = ['ROCK', 'PAPER', 'SCISSORS'];
 
-const getComputerChoice = () => {
-  let randomChoice = Math.floor(Math.random() * choice.length);
-  return choice[randomChoice];
-};
+let playerScore = 0;
+let computerScore = 0;
 
-const getHumanChoice = () => {
-  const humanChoice = prompt(
-    'Choice between: Rock, Paper Or Scissors'
-  ).toUpperCase();
-  return humanChoice;
-};
+btnRock.addEventListener('click', () => playRound('ROCK'));
+btnPaper.addEventListener('click', () => playRound('PAPER'));
+btnScissors.addEventListener('click', () => playRound('SCISSORS'));
 
-// ! Making the game last 5 round. Re adding stuff in this function and moving around stuff.
+const playRound = (humanChoice, computerChoice) => {
+  humanChoice = humanChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
 
-const playGame = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  const playRound = (humanChoice, computerChoice) => {
-    // Convert to uppercase just to be safe
-    humanChoice = humanChoice.toUpperCase();
-    computerChoice = computerChoice.toUpperCase();
-
-    if (humanChoice === computerChoice) {
-      console.log("It's a tie!");
-    } else if (
-      (humanChoice === 'PAPER' && computerChoice === 'ROCK') ||
-      (humanChoice === 'ROCK' && computerChoice === 'SCISSORS') ||
-      (humanChoice === 'SCISSORS' && computerChoice === 'PAPER')
-    ) {
-      playerScore++;
-      console.log(
-        `You win! ${humanChoice} beats ${computerChoice}. You: ${playerScore}, Computer: ${computerScore}`
-      );
-    } else {
-      computerScore++;
-      console.log(
-        `You lose! ${computerChoice} beats ${humanChoice}. You: ${playerScore}, Computer: ${computerScore}`
-      );
-    }
+  const getComputerChoice = () => {
+    let randomChoice = Math.floor(Math.random() * choice.length);
+    return choice[randomChoice];
   };
 
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-  }
-
-  if (playerScore > computerScore) {
-    console.log(
-      `You won the game! You: ${playerScore}, Computer: ${computerScore}`
-    );
-  } else if (computerScore > playerScore) {
-    console.log(
-      `Computer won the game! You: ${playerScore}, Computer: ${computerScore}`
-    );
+  if (humanChoice === computerChoice) {
+    resultDiv.textContent = `It's a tie!, You: ${playerScore}, Computer: ${computerScore}`;
+  } else if (
+    (humanChoice === 'PAPER' && computerChoice === 'ROCK') ||
+    (humanChoice === 'ROCK' && computerChoice === 'SCISSORS') ||
+    (humanChoice === 'SCISSORS' && computerChoice === 'PAPER')
+  ) {
+    playerScore++;
+    resultDiv.textContent = `You win! You: ${playerScore}, Computer: ${computerScore}`;
   } else {
-    console.log(
-      `The game is a tie! You: ${playerScore}, Computer: ${computerScore}`
-    );
+    computerScore++;
+    resultDiv.textContent = `You lose! You: ${playerScore}, Computer: ${computerScore}`;
   }
 };
-playGame();
